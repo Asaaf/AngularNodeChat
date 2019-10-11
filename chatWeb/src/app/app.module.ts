5f3353b8901770e61442a333b045144ecd83658c
Dasaaf <as.f09942@gmail.com>
2019-10-11 18:49:50 -0500

    Primera versión para un chat de ejemplo desarrollado con node y angular

---
+++ chatWeb/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DocumentListComponent } from './components/document-list/document-list.component';
+import { DocumentComponent } from './components/document/document.component';
+import { FormsModule } from '@angular/forms';
+
+const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DocumentListComponent,
+    DocumentComponent
+  ],
+  imports: [
+    BrowserModule,
+    SocketIoModule.forRoot(config),
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
